efc65028ef87766529aedef9574d516ccf70277a
Leandro <caponettopeppers@gmail.com>
2022-06-07 06:52:13 -0300

    simulador

---
+++ js/simulador.js
@@ -0,0 +1,82 @@
+//-----SIMULADOR DE ORDEN---------------------------
+
+
+
+
+function ingresarDatos(mensaje){
+    return prompt(mensaje)
+}
+
+function saludar(nombre){
+    alert("Hola " + nombre)
+}
+
+let nombre = ingresarDatos("Ingrese su nombre");
+
+saludar(nombre)
+let resu = 0;
+
+ 
+
+
+function calcularIva(valor){
+    console.log("El total con IVA es: $" + valor * 1.21);
+}  
+
+
+let plato = parseInt(ingresarDatos("\t ---Ingrese el plato deceado--- \n 1)- HANBUERGESAS $850.99. \n 2)- SNACK $750.99 . \n 3)- AVES $1500.99 .\n 4)- PIZZAS $950.99. \n 5)- POSTRES $715.99 \n 6)- POLLO FRITO $850.99 \n "));
+
+switch(plato){
+    case 1:
+        
+        calcularIva(850.99)
+        break;
+    case 2:
+        calcularIva(750.99);
+        
+        break;
+    case 3:
+        calcularIva(1500.99);
+        
+        break; 
+    case 4:
+        calcularIva(950.99);
+        break;
+    case 5:
+        calcularIva(715.99);
+        break;
+    case 6:
+        calcularIva(850.99);
+        break;
+    default:
+        alert("Igreso incorrecto" );
+        break;
+    
+                  
+}
+
+let fpago = parseInt(ingresarDatos("\t ---FORMAS DE PAGO--- \n 1)- TARJETA DE CREDITO.  \n 2)- MECADO PAGO.\n 3)- DEVITO."));
+    
+
+switch(fpago){
+    case 1:
+        alert("Su pago fue procesado correctamente");
+        break;
+    case 2:
+        alert("Su pago fue procesado correctamente");
+        break;
+    case 3:
+        alert("Su pago fue procesado correctamente");
+        break; 
+   
+    default:
+        alert("Igreso incorrecto" );
+        break;
+    
+                  
+}
+if(fpago < 1 || fpago > 4) {
+    prompt("\t ---FORMAS DE PAGO--- \n 1)- TARJETA DE CREDITO. \n 2)-DEVITO. \n 3)- MECADO PAGO.\n ");
+    alert("Su pago fue procesado correctamente")
+}
+
