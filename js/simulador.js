330eac16b34fb67ffdf2327380d78aadd692dcf5
Leandro <caponettopeppers@gmail.com>
2022-06-08 14:09:24 -0300

    modifico

--- js/simulador.js
+++ js/simulador.js
@@ -2,7 +2,7 @@
 
 
 
-
+/*
 function ingresarDatos(mensaje){
     return prompt(mensaje)
 }
@@ -16,67 +16,74 @@ let nombre = ingresarDatos("Ingrese su nombre");
 saludar(nombre)
 let resu = 0;
 
- 
+function Ploducto(plato, precio){
+    this.plato = plato;
+    this.precio = precio;
+}
+
+
+const platos = new Producto("hanburgesa", 850.99)
 
 
 function calcularIva(valor){
     console.log("El total con IVA es: $" + valor * 1.21);
 }  
-
-
-let plato = parseInt(ingresarDatos("\t ---Ingrese el plato deceado--- \n 1)- HANBUERGESAS $850.99. \n 2)- SNACK $750.99 . \n 3)- AVES $1500.99 .\n 4)- PIZZAS $950.99. \n 5)- POSTRES $715.99 \n 6)- POLLO FRITO $850.99 \n "));
-
-switch(plato){
-    case 1:
+*/
+/*
+let plato = parseInt(ingresarDatos("\t ---Ingrese el plato deceado--- \n 0)-SALIR \n 1)- HANBUERGESAS $850.99. \n 2)- SNACK $750.99 . \n 3)- AVES $1500.99 .\n 4)- PIZZAS $950.99. \n 5)- POSTRES $715.99 \n 6)- POLLO FRITO $850.99 \n "));
+while(plato !=0){
+    switch(plato){
+        case 1:
+            
+            calcularIva(850.99)
+            break;
+        case 2:
+            calcularIva(750.99);
+            
+            break;
+        case 3:
+            calcularIva(1500.99);
+            
+            break; 
+        case 4:
+            calcularIva(950.99);
+            break;
+        case 5:
+            calcularIva(715.99);
+            break;
+        case 6:
+            calcularIva(850.99);
+            break;
+        default:
+            alert("Igreso incorrecto" );
+            break;
         
-        calcularIva(850.99)
-        break;
-    case 2:
-        calcularIva(750.99);
+                    
+    }
+
+    let fpago = parseInt(ingresarDatos("\t ---FORMAS DE PAGO--- \n 1)- TARJETA DE CREDITO.  \n 2)- MECADO PAGO.\n 3)- DEVITO."));
         
-        break;
-    case 3:
-        calcularIva(1500.99);
+
+    switch(fpago){
+        case 1:
+            alert("Su pago fue procesado correctamente");
+            break;
+        case 2:
+            alert("Su pago fue procesado correctamente");
+            break;
+        case 3:
+            alert("Su pago fue procesado correctamente");
+            break; 
+    
+        default:
+            alert("Igreso incorrecto" );
+            break;
         
-        break; 
-    case 4:
-        calcularIva(950.99);
-        break;
-    case 5:
-        calcularIva(715.99);
-        break;
-    case 6:
-        calcularIva(850.99);
-        break;
-    default:
-        alert("Igreso incorrecto" );
-        break;
-    
-                  
-}
-
-let fpago = parseInt(ingresarDatos("\t ---FORMAS DE PAGO--- \n 1)- TARJETA DE CREDITO.  \n 2)- MECADO PAGO.\n 3)- DEVITO."));
-    
-
-switch(fpago){
-    case 1:
-        alert("Su pago fue procesado correctamente");
-        break;
-    case 2:
-        alert("Su pago fue procesado correctamente");
-        break;
-    case 3:
-        alert("Su pago fue procesado correctamente");
-        break; 
-   
-    default:
-        alert("Igreso incorrecto" );
-        break;
-    
-                  
-}
-if(fpago < 1 || fpago > 4) {
-    prompt("\t ---FORMAS DE PAGO--- \n 1)- TARJETA DE CREDITO. \n 2)-DEVITO. \n 3)- MECADO PAGO.\n ");
-    alert("Su pago fue procesado correctamente")
-}
-
+                    
+    }
+    if(fpago < 1 || fpago > 4) {
+        prompt("\t ---FORMAS DE PAGO--- \n 0)-SALIR \n 1)- TARJETA DE CREDITO. \n 2)-DEVITO. \n 3)- MECADO PAGO.\n ");
+        alert("Su pago fue procesado correctamente")
+    }
+}    
+*/
